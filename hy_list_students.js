12c1d0c53c62770695551bc7839f8ce9e53e7e2b
Matti Luukkainen <mluukkai@iki.fi>
2018-08-15 12:30:59 +0300

    stuff

--- hy_list_students.js
+++ hy_list_students.js
@@ -1,34 +1,20 @@
 const fs = require('fs')
 
-if (process.argv.length < 3) {
-  console.log('give csv file name as parameted')
-  process.exit(1)
+const toStudNr = (s) => {
+  if (s.student_number.startsWith('01') && s.student_number.length === 9 ) {
+    return s.student_number
+  }
+
+  if (s.login.startsWith('01') && s.login.length === 9) {
+    s.login
+  } 
+
+  return null
 }
 
-const toGrade = process.argv[2] 
+const studentsPassed = require('./suorittajat-9-8-18.json')
 
-const studentsPassed = require('./suorittajat-9-8-18.json').map(s => s.email.toLowerCase())
-
-const registeredLines = fs.readFileSync(toGrade, 'latin1').split('\n')
-
-registeredLines.shift()
-
-const registeredStudents = registeredLines.
-  filter(line => line.split(';').length > 2).
-  map(line => {
-  const parts = line.split(';')
-
-  if (parts[1].length<8) {
-    console.log('WARNING: ', parts)
-    return {}
-  }
-
-  return {
-    name: parts[0],
-    number: parts[1].startsWith('0') ? parts[1] : `0${parts[1]}`,
-    email: parts[2].replace('\r', '').toLowerCase()
-  }
-  }).filter(s => s.number)
+const studentNumbersPassed = studentsPassed.filter(s => toStudNr(s) != null).map(toStudNr)
 
 const files = fs.readdirSync('graded').filter(f => f !='.DS_Store')
 
@@ -37,29 +23,29 @@ const alreadyGraded = files.reduce((s, file) =>
   , []
 )
 
-const registeredPassed = registeredStudents
-  .filter(s => studentsPassed.includes(s.email))
+const passedNotGraded = studentNumbersPassed
+  .filter(s => !alreadyGraded.includes(s))
 
-const registeredPassedNotGraded = registeredPassed
-  .filter(s => !alreadyGraded.includes(s.number))
+console.log('passed', studentNumbersPassed.length)
+console.log('passedNotGraded', passedNotGraded.length)
 
-console.log('registeredPassed', registeredPassed.length)
-console.log('registeredPassedNotGraded', registeredPassedNotGraded.length)
 
-if (process.argv.length === 4) {
-  const registeredProblm = registeredStudents
-    .filter(s => !studentsPassed.includes(s.email))
-  console.log('registeredProblm', registeredProblm.length)
-  console.log(registeredProblm.map(s => s.email).join('\n'))
-  process.exit(0)
+if (passedNotGraded.length === 0 ) {
+  process.exit(1)
 }
 
-const filename = `graded/${toGrade.replace('.csv', '.txt')}`
+if (process.argv.length<3 ) {
+  console.log('give date as ddmmyy')
+  process.exit(1)
+}
+
+const filename = `graded/${process.argv[2]}`
 
 if (fs.existsSync(filename)) {
   console.log('transfer file already created?')
 } else {
-  const numbers = registeredPassedNotGraded.map(s => s.number).join('\n')
+  const numbers = passedNotGraded.join('\n')
   fs.writeFileSync(filename, numbers, 'utf8')
   fs.writeFileSync('student_numbers.txt', numbers, 'utf8')
+  console.log('transfer file created')
 }
